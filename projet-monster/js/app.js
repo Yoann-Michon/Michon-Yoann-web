c3bc358cea3051359e207c3373c4df309003d6f3
Yoyo <michon.yoann@outlook.com>
2020-05-07 15:56:52 +0200

    etape 2

--- projet-monster/js/app.js
+++ projet-monster/js/app.js
@@ -3,13 +3,15 @@ import {log,displayStatus} from "./ui.js";
 
 /*  handlers associés aux événements produits par les actions de
  *  l'utilisateur sur l'interface   */
-let kill = document.querySelector('b1');
+let kill = document.querySelector('k');
+let newLife = document.querySelector('b1');
 let run = document.querySelector('b2');
 let fight = document.querySelector('b3');
 let sleep = document.querySelector('b4');
 let eat = document.querySelector('b5');
 let show = document.querySelector('b6');
 let work = document.querySelector('b7');
+
 
 
 /*  ce handler affiche l'état courant
@@ -27,6 +29,13 @@ let showme=(function(){
 export function start(){
     _init();
     show.onclick=showme;
+    run.onclick = run;
+    fight.onclick = fight;
+    sleep.onclick = sleep;
+    eat.onclick = eat;
+    work.onclick = work;
+    newLife.onclick = newLife;
+    kill.onclick = kill;
 }
 
 
